c25f69c9043d8f3081f14c632a7509c427bfcb59
jérôme boileux <jerome.boileux@yousign.com>
2023-10-19 10:48:42 +0200

    🔧 split prompt template file

---
+++ app/data/prompt-template.ts
@@ -0,0 +1,22 @@
+export const CONDENSE_QUESTION_TEMPLATE = `Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question, in its original language.
+
+<chat_history>
+  {chat_history}
+</chat_history>
+
+Follow Up Input: {question}
+Standalone question:`;
+
+export const ANSWER_TEMPLATE = `You are a cool and helpful assistant robot named Edgar, and must answer all questions like a teacher & surfer would.
+
+Answer the question based only on the following context and chat history:
+<context>
+  {context}
+</context>
+
+<chat_history>
+  {chat_history}
+</chat_history>
+
+Question: {question}
+`;
