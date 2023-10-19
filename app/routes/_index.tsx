01d644c5d5cbe713b7da0a445dc3113078a651a2
Simon Robin <simon@simrobin.fr>
2023-10-19 13:48:46 +0200

    💫 persist doc uuid in URL when uploaded

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -3,11 +3,12 @@ import {
   type ActionFunctionArgs,
   type MetaFunction,
 } from '@remix-run/node';
-import { useActionData, useNavigation } from '@remix-run/react';
+import { useActionData, useNavigation, useSearchParams } from '@remix-run/react';
 
 import { UploadForm } from '~/components/UploadForm';
 import { ChatBox } from '~/components/ChatBox';
 import { upload } from '~/api/upload';
+import * as React from 'react';
 
 export async function action({ request }: ActionFunctionArgs) {
   const formData = await request.formData();
@@ -39,27 +40,35 @@ export const meta: MetaFunction = () => {
 export default function Index() {
   const navigation = useNavigation();
   const actionData = useActionData<typeof action>();
+  const [searchParams, setSearchParams] = useSearchParams();
 
-  const shouldShowUploadForm = !actionData?.ok;
+  React.useEffect(() => {
+    if (actionData?.docUUID) {
+      setSearchParams({ docUUID: actionData.docUUID });
+    }
+  }, [actionData?.docUUID, setSearchParams]);
+
+  const docUUID = searchParams.get('docUUID') || actionData?.docUUID;
+
   const isSubmitting = navigation.state === 'submitting';
 
   return (
     <div className="prose lg:prose-xl prose-slate container mx-auto p-10 bg-white shadow-md m-10 rounded-md">
       <header className="">
         <h1>Hey, my name is Edgar 👋</h1>
-        {shouldShowUploadForm ? (
-          <h2 className="text-gray-400">📬 Give me your document</h2>
-        ) : (
+        {docUUID ? (
           <h2 className="text-gray-400">
             💬 Ask me something about your document
           </h2>
+        ) : (
+          <h2 className="text-gray-400">📬 Give me your document</h2>
         )}
       </header>
 
-      {shouldShowUploadForm ? (
+      {docUUID ? (
+        <ChatBox docUUID={docUUID} />
+        ) : (
         <UploadForm isSubmitting={isSubmitting} />
-      ) : (
-        <ChatBox docUUID={actionData.docUUID} />
       )}
     </div>
   );
